27ad3e1605217f5ce2d0b32e60c667f1795ea50e
zemos <zemos_waltan@yahoo.pl>
2012-05-10 20:03:14 +0200

    fresh start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zemosowo::Application.config.secret_token = 'daaf0a07d5917fbe61026d671f2aa2ef1f1ab5f284eaabfc1be83f888cece151f497bd0d2df03bec4d5d43d0812b98a6b6760e48121724d40a2935811678aa9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Zemosowo::Application.config.session_store :cookie_store, key: '_zemosowo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Zemosowo::Application.config.session_store :active_record_store
